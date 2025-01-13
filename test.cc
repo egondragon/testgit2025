6985047c34754cde79eef2f537f643301a582047
egondragon <lamtoure@gmail.com>
2025-01-13 16:46:01 +0000

    First commit in testing branch

--- test.cc
+++ test.cc
@@ -1,6 +1,22 @@
 #include <string>
 #include <iostream>
 
+class myPerson {
+  std::string sz_firstname;
+  std::string sz_lastname;
+  
+public:
+  
+  myPerson(std::string sz_first, std::string sz_last) {
+    sz_firstname = sz_first;
+    sz_lastname = sz_last;    
+  }
+  
+  void m_display(void) const {
+    std::cout << sz_firstname << " " << sz_lastname << std::endl;
+  }
+};
+  
 void usage(std::string sz_name) {
   std::cout << "Usage :" << sz_name << " <firstname> <lastname>" << std::endl;
 }
@@ -12,11 +28,12 @@ int main(int ac, char **av) {
     usage(av[0]);
     return 1;
   }
-
-  std::string sz_firstname = av[1];
-  std::string sz_lastname = av[2];
-
-  std::cout << "Hi Mr " << sz_firstname << " - " << sz_lastname << std::endl;
+  myPerson *p = new myPerson(av[1], av[2]);;
   
+  std::cout << "Hi Mr ";
+  p->m_display();
+  
+  delete p;
+    
   return 0;
 }
